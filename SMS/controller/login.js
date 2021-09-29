0ae11aa4237e5eab09b6d061e703ad1855170b7c
saharshg29 <saharshg29@gmail.com>
2021-09-29 17:55:58 +0530

    tokens added

--- SMS/controller/login.js
+++ SMS/controller/login.js
@@ -11,7 +11,7 @@ class LogIn {
         const schema = Joi.object({
             email: Joi.string().min(6).max(255).required().email(),
             password: Joi.string().max(1024).min(8).required(),
-          });
+        });
         const { errr } = schema.validate(req.body);
         if (errr) return res.status(400).send("Validation failed");
 
@@ -24,9 +24,9 @@ class LogIn {
         if (!validPass) return res.status(400).send("Wrong username or password");
         else {
             console.log('Logged IN')
-            res.status(200).send('Logged IN')
             const token = await jwt.sign({ _id: userExist._id }, process.env.PRIVATE_TOKEN);
-            res.header('auth-token', token).send(token);
+            res.header('auth-token', token);
+            res.send(token)
         }
     }
 }
